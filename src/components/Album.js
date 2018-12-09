e296ceb4589d4b16b92722d28b05ad50efe76461
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2018-12-09 13:59:45 -0800

    Added player bar with previous and next buttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar'; 
 
 class Album extends Component {
 	constructor(props) {
@@ -52,6 +53,22 @@ class Album extends Component {
 		this.setState({ currentSongHover: null})
 	}
 
+	handlePrevClick() {
+		const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song); 
+		const newIndex = Math.max(0, currentIndex - 1); 
+		const newSong = this.state.album.songs[newIndex]; 
+		this.setSong(newSong); 
+		this.play(); 
+	}
+
+	handleNextClick() {
+		const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song); 
+		const newIndex = Math.max(0, currentIndex + 1); 
+		const newSong = this.state.album.songs[newIndex]; 
+		this.setSong(newSong); 
+		this.play(); 
+	}
+
 	render() {
 		return (
 			<section className="album"> 
@@ -94,6 +111,13 @@ class Album extends Component {
 						})}
 					</tbody>
 				</table>
+				<PlayerBar 
+					isPlaying={this.state.isPlaying} 
+					currentSong={this.state.currentSong} 
+					handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+					handlePrevClick={() => this.handlePrevClick()}
+					handleNextClick={() => this.handleNextClick()}
+					/>
 			</section>
 		);
 	}
